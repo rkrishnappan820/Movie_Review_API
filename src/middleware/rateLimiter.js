7a88ee8fd8ee03fe5d4f00992903b561ee4c9f57
Rahul Krishnappan <rkrishnappan820@gmail.com>
2026-04-28 10:01:41 -0400

    increased rate limit to 3 requests for every 10 mins

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -1,7 +1,7 @@
 import rateLimit from 'express-rate-limit';
 
 export const loginLimiter = rateLimit({
-  windowMs: 1 * 60 * 1000,
+  windowMs: 10 * 60 * 1000,
   limit: 3,
   handler: function (req, res, next) {
     const error = new Error('Exceeded max login attempts for given interval');
